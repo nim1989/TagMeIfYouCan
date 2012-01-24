5cce82d0ab7b7af4f6047fb132ebc67783f8fedd
Nicolas Dupont <npg.dupont@gmail.com>
2012-01-24 16:18:55 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TagMeIfYouCan::Application.config.secret_token = 'ffecae878288521fee6724dd9ce4c9c287f4300963b88bf880be987d378dc05cb1f0d77474ccfda39a37708f703cd178bc22dbc7f98e16484dccf5ecfae25f6b'

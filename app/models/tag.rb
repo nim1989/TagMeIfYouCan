2729e5159726f0486d8588ec6f94e325c72132b3
Nicolas Dupont <npg.dupont@gmail.com>
2012-02-18 19:05:57 +0100

    tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,9 +1,15 @@
 class Tag < ActiveRecord::Base
   has_many :tags_facebooks
   has_many :facebooks, :through => :tags_facebooks, :foreign_key => "facebook_identifier"
-  
+  before_create :generate_wiki_url_and_thumb
   validates :uri, :presence => true
 
+  def generate_wiki_url_and_thumb
+    movie = Movie.where(:uri => self.uri).first
+   self.wikipedia_url = movie.wikipedia_url
+   self.thumbnail = movie.thumbnail.gsub!('commons/thumb','en').gsub(/\/200px.*/, '')
+  end
+ 
   def retrieve_info
     # Append RDF information to people-movie.nt
     query = <<-QUERY

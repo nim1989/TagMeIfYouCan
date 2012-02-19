d20e45fc5a8f908c03ec028fbaa5912614e2a296
Nima Izadi <nim.izadi@gmail.com>
2012-02-19 14:58:43 +0100

    initializer dir

--- config/initializers/file.rb
+++ config/initializers/file.rb
@@ -1,3 +1,7 @@
+if ! File.directory? "app/assets/rdf"
+  Dir.mkdir('app/assets/rdf')
+end
+
 if !FileTest.exists?('app/assets/rdf/people-film.nt') 
   File.open('app/assets/rdf/people-film.nt', 'a')
 end
